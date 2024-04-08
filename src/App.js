cb9077f700f11f09e7c0c8c64b8eea4195b0c3b0
Riya Bansal <riyabansal@Riyas-MacBook-Pro.local>
2024-04-08 12:55:12 +0530

    Minor changes to test deployment

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ function App() {
   return (
     <div style={{ textAlign: 'center' }}>
       <h1>Welcome to My Simple React Static Site</h1>
-      <p>This is a basic static React application.</p>
+      <p>This is a basic static React application...</p>
     </div>
   );
 }
 
 export default App;
+
+
